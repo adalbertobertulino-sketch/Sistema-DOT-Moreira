473b7d161000dbd84f7e9eae460c667f617e05b0
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-21 16:52:12 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,43 +1,18 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } 
-from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
-// CONFIGURAÇÃO DO SEU FIREBASE
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBMr2MIbnPw7k3W6WVmWwY-Pa3VgG0z1qk",
   authDomain: "sistema-dot.firebaseapp.com",
   projectId: "sistema-dot",
-  storageBucket: "sistema-dot.appspot.com",
+  storageBucket: "sistema-dot.firebasestorage.app",
   messagingSenderId: "1003611331429",
-  appId: "1:1003611331429:web:bf8478cef6fecfa6059f8c"
+  appId: "1:1003611331429:web:2b55b32379b447e3059f8c"
 };
 
-// Inicializa
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-// LOGIN EMAIL
-window.loginEmail = function() {
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("senha").value;
-
-  signInWithEmailAndPassword(auth, email, senha)
-    .then(() => {
-      window.location.href = "dashboard.html";
-    })
-    .catch((error) => {
-      alert("Erro: " + error.message);
-    });
-}
-
-// LOGIN GOOGLE
-window.loginGoogle = function() {
-  signInWithPopup(auth, provider)
-    .then(() => {
-      window.location.href = "dashboard.html";
-    })
-    .catch((error) => {
-      alert("Erro: " + error.message);
-    });
-}
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
