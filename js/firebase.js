9cd6f5bb4686a592d18115148f21108246bf665d
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-22 20:43:32 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,8 @@
-// js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// >>>> COLEI O SEU SDK AQUI (o measurementId pode ficar, não atrapalha)
 const firebaseConfig = {
   apiKey: "AIzaSyBMr2MIbnPw7k3W6WVmWwY-Pa3VgG0z1qk",
   authDomain: "sistema-dot.firebaseapp.com",
@@ -11,10 +10,9 @@ const firebaseConfig = {
   storageBucket: "sistema-dot.firebasestorage.app",
   messagingSenderId: "1003611331429",
   appId: "1:1003611331429:web:b0a6cbf2d3578035059f8c",
-  measurementId: "G-F7Y437WQDH",
+  measurementId: "G-F7Y437WQDH"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
