d1c2a315da709c91c18856158809efda5ed15437
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-22 20:22:33 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,13 +3,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebas
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
+// >>>> COLEI O SEU SDK AQUI (o measurementId pode ficar, não atrapalha)
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID",
+  apiKey: "AIzaSyBMr2MIbnPw7k3W6WVmWwY-Pa3VgG0z1qk",
+  authDomain: "sistema-dot.firebaseapp.com",
+  projectId: "sistema-dot",
+  storageBucket: "sistema-dot.firebasestorage.app",
+  messagingSenderId: "1003611331429",
+  appId: "1:1003611331429:web:b0a6cbf2d3578035059f8c",
+  measurementId: "G-F7Y437WQDH",
 };
 
 export const app = initializeApp(firebaseConfig);
