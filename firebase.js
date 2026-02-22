d7fd18598d0ae59da93e83325c1be1bbcdc30038
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-22 14:19:14 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,22 @@
-// firebase.js — BASE DO FIREBASE (OBRIGATÓRIO)
+// firebase.js (SDK modular)
+// APAGUE TUDO e COLE
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// CONFIGURAÇÃO DO SEU PROJETO
-export const firebaseConfig = {
+// ✅ SUA CONFIG (a que você mandou)
+const firebaseConfig = {
   apiKey: "AIzaSyBMr2MIbnPw7k3W6WVmWwY-Pa3VgG0z1qk",
   authDomain: "sistema-dot.firebaseapp.com",
   projectId: "sistema-dot",
-  storageBucket: "sistema-dot.appspot.com",
+  storageBucket: "sistema-dot.firebasestorage.app",
   messagingSenderId: "1003611331429",
-  appId: "1:1003611331429:web:2b55b32379b447e3059f8c"
+  appId: "1:1003611331429:web:2b55b32379b447e3059f8c",
+  measurementId: "G-FS1CBVNFEG"
 };
 
-// INICIALIZA
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
