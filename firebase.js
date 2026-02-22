7c2ce0b489029ca56666bb63ee154309ee350baa
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-22 17:40:30 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,12 @@
-// firebase.js (COMPLETO)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// ⚠️ Use exatamente o config do seu Firebase Console (Web app)
 const firebaseConfig = {
   apiKey: "AIzaSyBMr2MIbnPw7k3W6WVmWwY-Pa3VgG0z1qk",
   authDomain: "sistema-dot.firebaseapp.com",
   projectId: "sistema-dot",
-  storageBucket: "sistema-dot.appspot.com", // ✅ geralmente é .appspot.com (não .firebasestorage.app)
+  storageBucket: "sistema-dot.appspot.com",
   messagingSenderId: "1003611331429",
   appId: "1:1003611331429:web:2b55b32379b447e3059f8c",
   measurementId: "G-FS1CBVNFEG"
