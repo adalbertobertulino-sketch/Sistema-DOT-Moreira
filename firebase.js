b6e64d7f267dde1e3203fbce7013e9fd2f305b6b
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-22 13:10:37 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
+// firebase.js — BASE DO FIREBASE (OBRIGATÓRIO)
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// CONFIGURAÇÃO DO SEU PROJETO
+export const firebaseConfig = {
+  apiKey: "AIzaSyBMr2MIbnPw7k3W6WVmWwY-Pa3VgG0z1qk",
+  authDomain: "sistema-dot.firebaseapp.com",
+  projectId: "sistema-dot",
+  storageBucket: "sistema-dot.appspot.com",
+  messagingSenderId: "1003611331429",
+  appId: "1:1003611331429:web:2b55b32379b447e3059f8c"
+};
+
+// INICIALIZA
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
