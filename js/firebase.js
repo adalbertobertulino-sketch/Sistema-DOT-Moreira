125151e53467a4a9548c0fb7536d103d34554537
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-22 21:24:56 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,6 @@
-// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
-// >>> COLE AQUI SEU firebaseConfig (o seu já está abaixo)
 const firebaseConfig = {
   apiKey: "AIzaSyBMr2MIbnPw7k3W6WVmWwY-Pa3VgG0z1qk",
   authDomain: "sistema-dot.firebaseapp.com",
@@ -16,4 +13,3 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
