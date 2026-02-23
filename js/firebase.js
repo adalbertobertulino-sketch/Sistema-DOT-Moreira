529f6d0e619f471776161d96fefd75f929bf6032
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-22 21:32:45 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { app } from "./firebase.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBMr2MIbnPw7k3W6WVmWwY-Pa3VgG0z1qk",
-  authDomain: "sistema-dot.firebaseapp.com",
-  projectId: "sistema-dot",
-  storageBucket: "sistema-dot.firebasestorage.app",
-  messagingSenderId: "1003611331429",
-  appId: "1:1003611331429:web:b0a6cbf2d3578035059f8c",
-  measurementId: "G-F7Y437WQDH"
-};
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const db = getFirestore(app);
