5bf20c72b29c9bf024f8462d04467a85c4e4c259
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-24 22:26:12 -0300

    Update superbase.js

--- js/superbase.js
+++ js/superbase.js
@@ -1,9 +1,10 @@
+// js/supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://ylgjxbhdekmwssspypwc.supabase.co";
-const SUPABASE_ANON_KEY = "COLE_AQUI_SUA_ANON_KEY";
+// 1) Cole AQUI sua URL do Supabase (Settings > API > Project URL)
+const SUPABASE_URL = "COLE_SUA_SUPABASE_URL_AQUI";
 
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+// 2) Cole AQUI sua anon public key (Settings > API > anon public)
+const SUPABASE_ANON_KEY = "COLE_SUA_ANON_KEY_AQUI";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
